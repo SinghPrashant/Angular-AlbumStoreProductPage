4f1cb9dd37fbb2f3d466dfbc75cd6c1a87d6eb21
Prashant Singh <prabhatk40@gmail.com>
2020-05-18 09:51:35 +0530

    Save

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from "@angular/core";
 import { Http, Response } from "@angular/http";
+import { Observable } from "rxjs/Observable";
 
 @Injectable()
 export class ProductService {
   _albumUrl = "../assets/album.json";
+
   constructor(private _http: Http) {}
+
+  getAlbum(id: number): Observable<any> {
+    return this._http.get(this._albumUrl);
+  }
 }
